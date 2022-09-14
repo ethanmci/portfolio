50aa3f0ef9121feb1998aaff3a796a680c2a4b1c
Ethan McIntyre <EthanMcIntyre@cmail.carleton.ca>
2022-09-14 17:52:36 -0400

    init

---
+++ components/Item.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="bg-white padding">
+
+  </div>
+</template>
+<script lang="ts">
+export default {
+
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <!-- hero opener -->
+    <div class="relative w-full bg-neutral-800 block" style="height: 700px">
+      <div class="m-auto w-96" style="transform">
+        <span class="bg-lime-700 text-orange-100 m-auto font-semibold text-2xl p-2 shadow-sm rounded-sm">Hello, my name is Ethan McIntyre</span>
+        <p class="mt-4 text-orange-100 text-center">Full Stack Web Developer, 100gecs enjoyer.</p>
+      </div>
+    </div>
+</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage',
+})
+</script>
