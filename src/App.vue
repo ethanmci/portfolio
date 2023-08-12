b8e2ad0d085b21fcfb32bd681349a6e7d632bd8e
Ethan McIntyre <ethan.sndr.mc@gmail.com>
2023-08-12 18:54:11 -0400

    added new google font

--- src/App.vue
+++ src/App.vue
@@ -13,18 +13,22 @@ import { RouterLink, RouterView } from 'vue-router'
   />
   <div
     id="main-container"
-    class="absolute w-3/4 h-3/4 bg-slate-50 p-8 border-2 border-emerald-500 rounded-lg shadow-lg transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+    class="absolute w-3/4 h-3/4 bg-slate-50/70 p-8 border-2 rounded-lg shadow-lg transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
   >
     <div class="grid grid-cols-4 h-full gap-4">
-      <div class="col-span-1 h-full p-4 rounded-lg shadow-md">
-        <h1 class="font-black text-7xl antialiased text-emerald-500 hover:text-emerald-700 transition-all">Ethan McIntyre</h1>
-        <h2 class="font-bold text-xl antialiased">Web Developer</h2>
-        <img class="relative rounded-xl my-4" src="./assets/photo_of_me.jpg">
-        <p>Hello, my name is Ethan! I'm a graduate from the Interactive Multimedia Design program at Carleton University with a specialization in Full-Stack Web Development</p>
+      <div class="relative col-span-1 h-full p-4 bg-slate-50 border-2 border-slate-500 rounded-lg shadow-md">
+        <div class="absolute top-0 w-full mt-4">
+          <h1 class="font-black font-bricolage 2xl:text-7xl text-5xl antialiased text-sky-900 hover:text-sky-700 transition-all mix-blend-hard-light drop-shadow-sm">Ethan McIntyre</h1>
+        </div>
+        <img class="rounded-full mt-24 mb-4 w-full" src="./assets/photo_of_me.jpg">
+        <p class="font-bricolage">Hello, my name is Ethan! I'm a graduate from the Interactive Multimedia Design program at Carleton University with a specialization in Full-Stack Web Development.</p>
       </div>
 
       <!-- contains nav menu -->
-      <div class="col-span-3 h-full border-2 p-4 border-slate-400 rounded-lg shadow-md">
+      <div class="col-span-3 h-full bg-slate-50 border-2 border-slate-500 p-4 rounded-lg shadow-md">
+        <ul class="flex flex-wrap font-bricolage">
+
+        </ul>
         <header>
           <div class="wrapper">
             <nav>
