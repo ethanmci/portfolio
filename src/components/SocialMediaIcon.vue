3f94da8235a466ff0fcde0b9eb462f5e932d0fed
Ethan McIntyre <ethan.sndr.mc@gmail.com>
2023-08-12 23:45:00 -0400

    contact vue, social media icon component

---
+++ src/components/SocialMediaIcon.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const props = defineProps<{
+    imgSrc: string,
+    hoverColor?: string
+}>()
+</script>
+
+<template>
+    <div>
+        <img :src="imgSrc"/>
+    </div>
+</template>
--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,9 +1,43 @@
 <script setup lang="ts">
-import MainContent from '@/components/MainContent.vue';
+import { ref } from 'vue'
+import type { Ref } from 'vue'
+
+const WEB3FORMS_ACCESS_KEY = import.meta.env.WEB3FORMS_ACCESS_KEY
+const name: Ref<string | undefined> = ref()
+const email: Ref<string | undefined> = ref()
+const subject: Ref<string | undefined> = ref()
+const message: Ref<string | undefined> = ref()
+
+function SubmitContact() {
+
+}
 </script>
 
 <template>
-  <main>
-    <MainContent/>
+  <main class="p-4 font-bricolage">
+    <h2 class="font-black 2xl:text-5xl text-3xl text-center antialiased text-sky-900 hover:text-sky-700 transition-all mix-blend-hard-light drop-shadow-sm">Contact!</h2>
+    <p class="text-sm text-center text-slate-500">(feel free to contact me on linkedin also)</p>
+    <br>
+    <form class="grid grid-cols-2">
+      <div class="md:col-span-1 col-span-2 md:mr-4 mb-2">
+        <label for="name">*Name</label>
+        <input v-model="name" name="name" class="border-2 border-slate-500 p-2 rounded-md block w-full" type="text">
+      </div>
+      <div class="md:col-span-1 col-span-2 mb-2">
+        <label for="email">*Email</label>
+        <input v-model="email" name="email" class="border-2 border-slate-500 p-2 rounded-md block w-full" type="email" placeholder="example@email.com">
+      </div>
+      <div class="col-span-2 mb-2">
+        <label for="subject">Subject</label>
+        <input v-model="subject" name="subject" class="border-2 border-slate-500 p-2 rounded-md block w-full" type="text" placeholder="Subject">
+      </div>
+      <div class="col-span-2 mb-2">
+        <label for="email">*Message</label>
+        <textarea v-model="message" name="message" class="border-2 border-slate-500 p-2 rounded-md block w-full h-28" type="text" placeholder="example@email.com"></textarea>
+      </div>
+      <div class="col-span-2 mb-2 flex flex-col items-center">
+        <button type="submit" class="p-4 bg-orange-600 rounded-md text-white font-bold">Submit</button>
+      </div>
+    </form>
   </main>
 </template>
