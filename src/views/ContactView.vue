936aaa2a50b008ad51fab82495c6a0a8b196eb9c
Ethan McIntyre <ethan.sndr.mc@gmail.com>
2023-08-14 01:54:06 -0400

    working on some validation stuff

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -2,41 +2,76 @@
 import { ref } from 'vue'
 import type { Ref } from 'vue'
 
-const WEB3FORMS_ACCESS_KEY = import.meta.env.WEB3FORMS_ACCESS_KEY
-const name: Ref<string | undefined> = ref()
-const email: Ref<string | undefined> = ref()
-const subject: Ref<string | undefined> = ref()
-const message: Ref<string | undefined> = ref()
-
-function SubmitContact() {
-
+interface FormType {
+  value: string
+  valid: boolean
 }
+
+const WEB3FORMS_ACCESS_KEY = import.meta.env.WEB3FORMS_ACCESS_KEY
+const name: Ref<string> = ref('')
+const email: Ref<string> = ref('')
+const subject: Ref<string> = ref('')
+const message: Ref<string> = ref('')
+
+function SubmitContact() {}
 </script>
 
 <template>
   <main class="p-4 font-bricolage">
-    <h2 class="font-black 2xl:text-5xl text-3xl text-center antialiased text-sky-900 hover:text-sky-700 transition-all mix-blend-hard-light drop-shadow-sm">Contact!</h2>
+    <h2
+      class="font-black 2xl:text-5xl text-3xl text-center antialiased text-sky-900 hover:text-sky-700 transition-all mix-blend-hard-light drop-shadow-sm"
+    >
+      Contact!
+    </h2>
     <p class="text-sm text-center text-slate-500">(feel free to contact me on linkedin also)</p>
-    <br>
+    <br />
     <form class="grid grid-cols-2">
       <div class="md:col-span-1 col-span-2 md:mr-4 mb-2">
         <label for="name">*Name</label>
-        <input v-model="name" name="name" class="border-2 border-slate-500 p-2 rounded-md block w-full" type="text">
+        <input
+          v-model="name"
+          name="name"
+          :class="{ 'border-slate-500': name, 'border-red-500': !name }"
+          class="border-2 p-2 rounded-md block w-full"
+          type="text"
+        />
       </div>
       <div class="md:col-span-1 col-span-2 mb-2">
         <label for="email">*Email</label>
-        <input v-model="email" name="email" class="border-2 border-slate-500 p-2 rounded-md block w-full" type="email" placeholder="example@email.com">
+        <input
+          v-model="email"
+          name="email"
+          :class="{ 'border-slate-500': email, 'border-red-500': !email }"
+          class="border-2 border-slate-500 p-2 rounded-md block w-full"
+          type="email"
+          placeholder="example@email.com"
+        />
       </div>
       <div class="col-span-2 mb-2">
         <label for="subject">Subject</label>
-        <input v-model="subject" name="subject" class="border-2 border-slate-500 p-2 rounded-md block w-full" type="text" placeholder="Subject">
+        <input
+          v-model="subject"
+          name="subject"
+          class="border-2 border-slate-500 p-2 rounded-md block w-full"
+          type="text"
+          placeholder="Subject"
+        />
       </div>
       <div class="col-span-2 mb-2">
         <label for="email">*Message</label>
-        <textarea v-model="message" name="message" class="border-2 border-slate-500 p-2 rounded-md block w-full h-28" type="text" placeholder="example@email.com"></textarea>
+        <textarea
+          v-model="message"
+          name="message"
+          :class="{ 'border-slate-500': message, 'border-red-500': !message }"
+          class="border-2 border-slate-500 p-2 rounded-md block w-full h-28"
+          type="text"
+          placeholder="example@email.com"
+        ></textarea>
       </div>
       <div class="col-span-2 mb-2 flex flex-col items-center">
-        <button type="submit" class="p-4 bg-orange-600 rounded-md text-white font-bold">Submit</button>
+        <button type="submit" class="p-4 bg-orange-600 rounded-md text-white font-bold">
+          Submit
+        </button>
       </div>
     </form>
   </main>
