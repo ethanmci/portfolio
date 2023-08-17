9e14f64b1959f145196da02c34ce53aced2057a2
Ethan McIntyre <ethan.sndr.mc@gmail.com>
2023-08-17 14:57:54 -0400

    progress disclaimer

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { RouterLink, RouterView, useRouter } from 'vue-router'
 
-const route = useRouter();
+const route = useRouter()
 </script>
 
 <template>
@@ -14,6 +14,29 @@ const route = useRouter();
     src="./assets/name-stacked.png"
   />
   <div
+    class="absolute z-10 w-1/3 flex items-center justify-center p-3 rounded-md bg-yellow-600/50 top-2 left-1/2 transform -translate-x-1/2"
+  >
+    <div class="inline-block">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="w-6 h-6 mr-2 inline"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z"
+        />
+      </svg>
+      <p class="inline">
+        This site is currently under construction, thanks for checking it out though!
+      </p>
+    </div>
+  </div>
+  <div
     id="main-container"
     class="absolute md:w-5/6 md:h-5/6 md:m-0 h-full w-full overflow-auto bg-slate-50/70 p-4 border-2 rounded-lg shadow-lg transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 font-bricolage"
   >
@@ -25,7 +48,7 @@ const route = useRouter();
           <h1
             class="font-black 2xl:text-5xl text-6xl antialiased text-sky-900 hover:text-sky-700 transition-all mix-blend-hard-light drop-shadow-sm"
           >
-            Ethan<br/>McIntyre
+            Ethan<br />McIntyre
           </h1>
         </div>
         <img
@@ -64,7 +87,10 @@ const route = useRouter();
             <li class="col-span-1">
               <RouterLink
                 to="/"
-                :class="{ 'bg-sky-600 border-sky-600 text-white': route.currentRoute.value.path == '/', 'border-slate-200': route.currentRoute.value.path != '/' }"
+                :class="{
+                  'bg-sky-600 border-sky-600 text-white': route.currentRoute.value.path == '/',
+                  'border-slate-200': route.currentRoute.value.path != '/'
+                }"
                 class="w-full text-center font-bold inline-block p-2 border-x-2 border-t-2 rounded-t-full hover:text-white hover:bg-sky-600 hover:border-sky-600 transition-all"
                 >Home</RouterLink
               >
@@ -72,7 +98,11 @@ const route = useRouter();
             <li class="col-span-1">
               <RouterLink
                 to="/resume"
-                :class="{ 'bg-sky-600 border-sky-600 text-white': route.currentRoute.value.path == '/resume', 'border-slate-200': route.currentRoute.value.path != '/resume' }"
+                :class="{
+                  'bg-sky-600 border-sky-600 text-white':
+                    route.currentRoute.value.path == '/resume',
+                  'border-slate-200': route.currentRoute.value.path != '/resume'
+                }"
                 class="w-full text-center font-bold inline-block p-2 border-x-2 border-t-2 rounded-t-full hover:text-white hover:bg-sky-600 hover:border-sky-600 transition-all"
                 >Resume</RouterLink
               >
@@ -80,14 +110,18 @@ const route = useRouter();
             <li class="col-span-1">
               <RouterLink
                 to="/contact"
-                :class="{ 'bg-sky-600 border-sky-600 text-white': route.currentRoute.value.path == '/contact', 'border-slate-200': route.currentRoute.value.path != '/contact' }"
+                :class="{
+                  'bg-sky-600 border-sky-600 text-white':
+                    route.currentRoute.value.path == '/contact',
+                  'border-slate-200': route.currentRoute.value.path != '/contact'
+                }"
                 class="w-full text-center font-bold inline-block p-2 border-x-2 border-t-2 rounded-t-full hover:text-white hover:bg-sky-600 hover:border-sky-600 transition-all"
                 >Contact</RouterLink
               >
             </li>
           </ul>
         </header>
-        <RouterView class="border-t-2 border-slate-200  font-bricolage p-4 text-stone-800"/>
+        <RouterView class="border-t-2 border-slate-200 font-bricolage p-4 text-stone-800" />
       </div>
     </div>
   </div>
