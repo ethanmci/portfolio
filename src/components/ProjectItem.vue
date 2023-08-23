4fb83ac2f4aa22f2dc076cbdb0b379b14498dd8e
Ethan McIntyre <ethan.sndr.mc@gmail.com>
2023-08-23 01:23:54 -0400

    defaults for resume + project items

--- src/components/ProjectItem.vue
+++ src/components/ProjectItem.vue
@@ -1,6 +1,24 @@
-<script setup lang="ts"></script>
-<template>
-    <div>
+<script setup lang="ts">
+export interface Props {
+    cardImage?: string
+    title?: string
+    year?: string
+    description?: string
+}
 
-    </div>
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const props = withDefaults(defineProps<Props>(), {
+    cardImage: '../assets/project_default.jpg',
+    title: 'Project Title',
+    year: '202X',
+    projectDescription: 'No project description.'
+})
+
+</script>
+<template>
+<div class="md:col-span-1 col-span-full">
+    <!-- cover image -->
+    <img :src="cardImage">
+    <p class="truncate">{{ description }}</p>
+</div>
 </template>
--- src/components/ResumeItem.vue
+++ src/components/ResumeItem.vue
@@ -1,9 +1,24 @@
 <script setup lang="ts">
+export interface Props {
+    role?: string
+    location?: string,
+    startDate?: string,
+    endDate?: string,
+    description?: string
+}
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const props = withDefaults(defineProps<Props>(), {
+
+})
 
 </script>
 <template>
-<div>
-    
+<div >
+    {{ role }}
+    {{ location }}
+    {{ startDate }}
+    {{ endDate }}
+    <p class="truncate">{{ description }}</p>
 </div>
 </template>
