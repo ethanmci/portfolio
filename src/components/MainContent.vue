81f9a25d308751b2bd50560f9672433945153524
Ethan McIntyre <ethan.sndr.mc@gmail.com>
2023-09-06 03:39:42 -0400

    more project page work

--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -1,6 +1,40 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+import type { Ref } from 'vue'
+import ProjectItem from './ProjectItem.vue'
+export interface Project {
+  title: string
+  image: undefined
+  description?: string
+  year: string
+}
+
+
+const projects: Ref<Array<Project>> = ref([
+  {
+    title: 'Capy Island',
+    image: undefined,
+    description: 'A party game where players, who are capybaras, play mini-games on an island to collect hats',
+    year: '2023'
+  },
+  {
+    title: 'Capy Island',
+    image: undefined,
+    description: '',
+    year: '2023'
+  },
+])
 </script>
 
 <template>
-  <p>Landing page here!!</p>
+  <div>
+    <ProjectItem
+      v-for="(project, i) in projects"
+      v-bind:title="project.title"
+      v-bind:card-image="project.image"
+      v-bind:description="project.description"
+      v-bind:year="project.year"
+      v-bind:key="i"
+    />
+  </div>
 </template>
--- src/components/ProjectItem.vue
+++ src/components/ProjectItem.vue
@@ -1,24 +1,29 @@
 <script setup lang="ts">
+import defaultImg from '@/assets/project_default.jpg'
 export interface Props {
-    cardImage?: string
+    cardImage?: string | undefined
     title?: string
     year?: string
     description?: string
+    tags?: Array<string>
+    link?: string
 }
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const props = withDefaults(defineProps<Props>(), {
-    cardImage: '../assets/project_default.jpg',
+    cardImage: defaultImg,
     title: 'Project Title',
     year: '202X',
-    projectDescription: 'No project description.'
+    description: 'No project description.'
 })
 
 </script>
 <template>
-<div class="md:col-span-1 col-span-full">
+<div class="md:col-span-1 col-span-full min-h-[24] p-2 border-2 border-slate-500 rounded-lg shadow-md">
     <!-- cover image -->
-    <img :src="cardImage">
+    <img :src="cardImage" class="object-contain h-44 w-44 mx-auto rounded-md">
+    <h2 class="font-bold">{{ title }}</h2>
+    <h3>{{ year }}</h3>
     <p class="truncate">{{ description }}</p>
 </div>
 </template>
