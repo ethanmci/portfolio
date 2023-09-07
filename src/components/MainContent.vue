69484643b60c4a49a73357d6715a6f6f26787ae7
Ethan McIntyre <ethan.sndr.mc@gmail.com>
2023-09-07 01:58:21 -0400

    tags!

--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -1,41 +1,21 @@
 <script setup lang="ts">
+// vite-env.d.ts
+/// <reference types="@modyfi/vite-plugin-yaml/modules" />
 import { ref } from 'vue'
 import type { Ref } from 'vue'
 import ProjectItem from './ProjectItem.vue'
+import ProjectsYAML from '@/assets/projects.yaml'
+
 export interface Project {
   title: string
   image: undefined
   description?: string
   year: string
+  tags: Array<string>
+  link: string
 }
 
-
-const projects: Ref<Array<Project>> = ref([
-  {
-    title: 'Capy Island',
-    image: undefined,
-    description: 'A party game where players, who are capybaras, play mini-games on an island to collect hats.',
-    year: '2023'
-  },
-  {
-    title: 'Capy Island',
-    image: undefined,
-    description: '',
-    year: '2023'
-  },
-  {
-    title: 'Capy Island',
-    image: undefined,
-    description: '',
-    year: '2023'
-  },
-  {
-    title: 'Capy Island',
-    image: undefined,
-    description: '',
-    year: '2023'
-  },
-])
+const projects: Ref<Array<Project>> = ref([...ProjectsYAML[0].projects])
 </script>
 
 <template>
@@ -43,9 +23,11 @@ const projects: Ref<Array<Project>> = ref([
     <ProjectItem
       v-for="(project, i) in projects"
       v-bind:title="project.title"
+      v-bind:year="project.year"
+      v-bind:link="project.link"
       v-bind:card-image="project.image"
       v-bind:description="project.description"
-      v-bind:year="project.year"
+      v-bind:tags="project.tags"
       v-bind:key="i"
     />
   </div>
--- src/components/ProjectItem.vue
+++ src/components/ProjectItem.vue
@@ -9,20 +9,32 @@ export interface Props {
   link?: string
 }
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const props = withDefaults(defineProps<Props>(), {
   cardImage: defaultImg,
   title: 'Project Title',
   year: '202X',
   description: 'No project description.'
 })
+
+const TagColour = (tag: string) => {
+  switch (tag) {
+    case 'Web':
+      return 'bg-orange-600'
+    case 'Game Development':
+      return 'bg-green-600'
+    case 'School':
+      return 'bg-red-600'
+    default: 
+      return 'bg-sky-600'
+  }
+}
  
 const placeholderFn = () => { console.log('accessible') }
 </script>
 <template>
   <a href="#" :on-click="() => placeholderFn()">
     <div
-      class="md:col-span-1 col-span-full h-72 p-2 border-2 border-slate-500 rounded-lg shadow-md hover:bg-slate-300 cursor-pointer"
+      class="md:col-span-1 col-span-full h-80 p-2 border-2 border-slate-500 rounded-lg shadow-md hover:bg-slate-300 cursor-pointer"
     >
       <!-- cover image -->
       <img :src="cardImage" class="object-cover h-44 w-full mx-auto rounded-md mb-2" />
@@ -30,8 +42,8 @@ const placeholderFn = () => { console.log('accessible') }
         <h2 class="font-bold col-span-1">{{ title }}</h2>
         <h3 class="col-span-1 text-right">{{ year }}</h3>
       </div>
-
-      <p class="text-ellipsis">{{ description }}</p>
+      <span v-for="(tag, i) in tags" v-bind:key="i" class="text-white p-1 mr-1 rounded-md" :class="TagColour(tag)">{{ tag }}</span>
+      <p class="mt-2 text-ellipsis">{{ description }}</p>
     </div>
   </a>
 </template>
